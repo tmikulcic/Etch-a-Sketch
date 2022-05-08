ec8f39c0a5101928de89ab6b622b104ea805669c
Tomislav <tom.mikulcic@gmail.com>
2022-05-08 02:13:28 +0200

    Add functionality to change grid color over mouseover

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ board.style.gridTemplateRows = 'repeat(16, 1fr)';
 
 for (let i = 0; i < 256; i++) {
   let square = document.createElement('div');
-  square.style.backgroundColor = 'gray';
+  square.style.backgroundColor = 'white';
+  square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = 'black';
+  });
   board.insertAdjacentElement('beforeend', square);
 }
