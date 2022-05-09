41e95347f834fa7ccd35ea3d9d8ff05511a90507
Tomislav <tom.mikulcic@gmail.com>
2022-05-09 16:25:04 +0200

    Add functionality for random color pick by Random button

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let board = document.querySelector('.board');
+let color = 'black';
 board.style.gridTemplateColumns = 'repeat(16, 1fr)';
 board.style.gridTemplateRows = 'repeat(16, 1fr)';
 
 for (let i = 0; i < 256; i++) {
   let square = document.createElement('div');
-  square.style.backgroundColor = 'white';
   square.addEventListener('mouseover', () => {
-    square.style.backgroundColor = 'black';
+    square.style.backgroundColor = color;
   });
   board.insertAdjacentElement('beforeend', square);
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
